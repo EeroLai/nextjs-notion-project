9caf88e2755af75054fbb8c29b63da0d29e09ef7
EeroLai <84887419+EeroLai@users.noreply.github.com>
2021-09-19 23:29:18 +0800

    Eero Lai - 20210919

--- pages/components/list.js
+++ pages/components/list.js
@@ -1,14 +1,21 @@
 import {
-    Navbar,
-    Nav,
-    NavDropdown,
-    Container,
+    ListGroup
 } from "react-bootstrap";
 
 export default function List() {
     return (
-        <Container>
-
-        </Container>
+        <div style={{margin:'2%'}}>
+            <ListGroup defaultActiveKey="#link1">
+                <ListGroup.Item action href="#link1">
+                    Link 1
+                </ListGroup.Item>
+                <ListGroup.Item action href="#link2" disabled>
+                    Link 2
+                </ListGroup.Item>
+                <ListGroup.Item action>
+                    This one is a button
+                </ListGroup.Item>
+            </ListGroup>
+        </div>
     )
 }
