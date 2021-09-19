2987cc32b94e6f0823de87d114d070a6357eb7c4
EeroLai <84887419+EeroLai@users.noreply.github.com>
2021-09-19 17:41:14 +0800

    first commit

---
+++ pages/components/list.js
@@ -0,0 +1,14 @@
+import {
+    Navbar,
+    Nav,
+    NavDropdown,
+    Container,
+} from "react-bootstrap";
+
+export default function List() {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
