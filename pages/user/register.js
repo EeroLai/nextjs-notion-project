465252c78b933afe1a235405fe2ab2dc5c861457
EeroLai <84887419+EeroLai@users.noreply.github.com>
2021-10-17 22:22:20 +0800

    EeroLai-20211017

--- pages/user/register.js
+++ pages/user/register.js
@@ -8,41 +8,39 @@ import {
 
 firebase();
 export default function register() {
-    const [email , setEmail ] = useState('');
-    const [password , setPassword ] = useState('');
-    const [checkPassword , setCheckPassword ] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [checkPassword, setCheckPassword] = useState('');
 
-    function registBtn(){
-        if(password == checkPassword){
-            
-        }else{
+    function registBtn() {
+        if (password == checkPassword) {
+
+        } else {
             alert('密碼不一致');
             return false;
         }
     }
-    
+
     return (
-        <Layout>
-            <div style={{ marginTop: '5%' }}>
-                <h1 style={{ textAlign: 'center' }}>Register</h1>
-                <Form style={{ margin: '0 auto', padding: '12px', width: '40%' }}>
-                    <Form.Group size="lg" controlId="email" style={{ margin: "1%" }}>
-                        <Form.Label style={{ textAlign: 'left' }}>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
-                    <Form.Group size="lg" controlId="password" style={{ margin: "1%" }}>
-                        <Form.Label style={{ textAlign: 'left' }}>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" autoComplete="off" onChange={e=> setPassword(e.target.value)}/>
-                    </Form.Group>
-                    <Form.Group size="lg" controlId="checkPassword" style={{ margin: "1%" }}>
-                        <Form.Label style={{ textAlign: 'left' }}>Check Password</Form.Label>
-                        <Form.Control type="password" placeholder="Check Password" autoComplete="off" />
-                    </Form.Group>
-                    <Button style={{ float: 'right', marginTop: '5%' }} onClick={registBtn}>
-                        Register
-                    </Button>
-                </Form>
-            </div>
-        </Layout>
+        <div style={{ marginTop: '5%' }}>
+            <h1 style={{ textAlign: 'center' }}>Register</h1>
+            <Form style={{ margin: '0 auto', padding: '12px', width: '40%' }}>
+                <Form.Group size="lg" controlId="email" style={{ margin: "1%" }}>
+                    <Form.Label style={{ textAlign: 'left' }}>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group size="lg" controlId="password" style={{ margin: "1%" }}>
+                    <Form.Label style={{ textAlign: 'left' }}>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" autoComplete="off" onChange={e => setPassword(e.target.value)} />
+                </Form.Group>
+                <Form.Group size="lg" controlId="checkPassword" style={{ margin: "1%" }}>
+                    <Form.Label style={{ textAlign: 'left' }}>Check Password</Form.Label>
+                    <Form.Control type="password" placeholder="Check Password" autoComplete="off" />
+                </Form.Group>
+                <Button style={{ float: 'right', marginTop: '5%' }} onClick={registBtn}>
+                    Register
+                </Button>
+            </Form>
+        </div>
     )
 }
